04ee293505ca04f556aae67b32b403051c963c62
unknown <nkosimova06@gmail.com>
2020-04-20 22:26:09 +0500

    final code

--- Program.cs
+++ Program.cs
@@ -4,12 +4,15 @@ namespace CalculateMethods
 {
     class Program
     {
-        delegate U Operation<T, U>(T x, T y);
+        delegate T Operation<T>(T x, T y);
+        delegate U Opperation<T,U> (T x, T y);
         static void Main(string[] args)
         {
-            Operation<int,double> oper;
+            Operation<int> oper;
+            Opperation<int, double> opper;
             int x1, x2;
             char operation;
+            double result;
             
             Console.WriteLine("Enter numbers:");
             Console.Write("x1 = ");
@@ -23,33 +26,46 @@ namespace CalculateMethods
             switch(operation)
             {
                 case '+':
-                    oper = Addition<int, double>;
+                    oper = Addition<int>;
+                    result = oper(x1,x2);
+                    Console.WriteLine($"result = {result}");
                     break;
+                
                 case '-':
-                    oper = Subtraction<int, double>;
+                    oper = Subtraction<int>;
+                    result = oper(x1,x2);
+                    Console.WriteLine($"result = {result}");
                     break;
+                
                 case '*':
-                    oper = Multiplication<int, double>;
+                    oper = Multiplication<int>;
+                    result = oper(x1,x2);
+                    Console.WriteLine($"result = {result}");
                     break;
+                
                 case '/':
-                    oper = Division<int, double>;
+                    opper = Division<int, double>;
+                    result = Math.Round(opper(x1,x2),4);
+                    if (x2 == 0)
+                        Console.WriteLine("На 0 делить нельзя!!!");
+                    else
+                        Console.WriteLine($"result = {result}");
                     break;
+                
                 default:
                     Console.WriteLine("Incorrect operation");
                     break;
             }
-
-
         }
-        public static U Addition<T,U>(T x1, T x2)
+        public static T Addition<T>(T x1, T x2)
         {
             return (dynamic)x1 + (dynamic)x2;
         }
-        public static U Subtraction<T,U>(T x1, T x2)
+        public static T Subtraction<T>(T x1, T x2)
         {
             return (dynamic)x1 + (dynamic)x2;
         }
-        public static U Multiplication<T,U>(T x1, T x2)
+        public static T Multiplication<T>(T x1, T x2)
         {
             return (dynamic)x1 * (dynamic)x2;
         }
@@ -57,7 +73,6 @@ namespace CalculateMethods
         {
             if ((dynamic)x2 == 0)
             {
-                Console.WriteLine("На 0 делить нельзя!!!");
                 return (dynamic)0;
             }
             else 
