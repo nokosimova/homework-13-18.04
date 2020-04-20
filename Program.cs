15638794342ff54302d458d1d0a60e550362ad3e
unknown <nkosimova06@gmail.com>
2020-04-20 19:56:18 +0500

    create project, initialize methods

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CalculateMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static T Addition<T>(T x1, T x2)
+        {        }
+        public static T Subtraction<T>(T x1, T x2)
+        {        }
+        public static T Multiplication<T>(T x1, T x2)
+        {        }
+        public static T Division<T>(T x1, T x2)
+        {        }
+    }
+}
