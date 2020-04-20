dff41325d6bc9b9427a089cf5a803ae47fc67c66
unknown <nkosimova06@gmail.com>
2020-04-20 20:26:36 +0500

    fix all (+ - * /) methods

--- Program.cs
+++ Program.cs
@@ -9,12 +9,28 @@ namespace CalculateMethods
 
         }
         public static T Addition<T>(T x1, T x2)
-        {        }
+        {
+            return (dynamic)x1 + (dynamic)x2;
+        }
         public static T Subtraction<T>(T x1, T x2)
-        {        }
+        {
+            return (dynamic)x1 + (dynamic)x2;
+        }
         public static T Multiplication<T>(T x1, T x2)
-        {        }
+        {
+            return (dynamic)x1 * (dynamic)x2;
+        }
         public static T Division<T>(T x1, T x2)
-        {        }
+        {
+            if ((dynamic)x2 == 0)
+            {
+                Console.WriteLine("На 0 делить нельзя!!!");
+                return (dynamic)0;
+            }
+            else 
+            {
+                return (dynamic)x1 / (dynamic)x2;
+            }
+        }
     }
 }
