d31165260534353059b7d6e5326bb0df44e8156a
unknown <nkosimova06@gmail.com>
2020-04-20 21:51:37 +0500

    start writing the Main method

--- Program.cs
+++ Program.cs
@@ -4,23 +4,56 @@ namespace CalculateMethods
 {
     class Program
     {
+        delegate U Operation<T, U>(T x, T y);
         static void Main(string[] args)
         {
+            Operation<int,double> oper;
+            int x1, x2;
+            char operation;
+            
+            Console.WriteLine("Enter numbers:");
+            Console.Write("x1 = ");
+            x1 = int.Parse(Console.ReadLine());
+
+            Console.Write("x2 = ");
+            x2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Choose operation: + , - , * , / ");
+            operation = char.Parse(Console.ReadLine());
+            switch(operation)
+            {
+                case '+':
+                    oper = Addition<int, double>;
+                    break;
+                case '-':
+                    oper = Subtraction<int, double>;
+                    break;
+                case '*':
+                    oper = Multiplication<int, double>;
+                    break;
+                case '/':
+                    oper = Division<int, double>;
+                    break;
+                default:
+                    Console.WriteLine("Incorrect operation");
+                    break;
+            }
+
 
         }
-        public static T Addition<T>(T x1, T x2)
+        public static U Addition<T,U>(T x1, T x2)
         {
             return (dynamic)x1 + (dynamic)x2;
         }
-        public static T Subtraction<T>(T x1, T x2)
+        public static U Subtraction<T,U>(T x1, T x2)
         {
             return (dynamic)x1 + (dynamic)x2;
         }
-        public static T Multiplication<T>(T x1, T x2)
+        public static U Multiplication<T,U>(T x1, T x2)
         {
             return (dynamic)x1 * (dynamic)x2;
         }
-        public static T Division<T>(T x1, T x2)
+        public static U Division<T,U>(T x1, T x2)
         {
             if ((dynamic)x2 == 0)
             {
